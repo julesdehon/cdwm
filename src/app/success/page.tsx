d0da8c19653a5ed871c21b3211afb4f284f6a7a7
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2024-11-19 23:17:01 +0000

    Use img rather than Image for gif

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,6 +1,5 @@
 import styles from "@/app/page.module.css";
 import Link from 'next/link'
-import Image from "next/image";
 
 export default function SuccessPage() {
   return (
@@ -10,7 +9,7 @@ export default function SuccessPage() {
           <h1>Success!</h1>
           <p>You have successfully registered your choices from Come Dine With Me! We look forward to welcoming you at The Westmark</p>
         </div>
-        <Image src={"https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3Y3ZmZ2M3RndTNwaHdocTAxcTdhODk0ZWFvcnlvY2ZtNWF4NXF1biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13LlAxmDwAkopO/giphy.gif"}  alt={"A GIF showing a happy chef"}/>
+        <img src={"https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3Y3ZmZ2M3RndTNwaHdocTAxcTdhODk0ZWFvcnlvY2ZtNWF4NXF1biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13LlAxmDwAkopO/giphy.gif"}  alt={"A GIF showing a happy chef"}/>
         <Link style={{display: "block", margin: "20px auto"}} className={styles.button} href={"/"}>Back Home</Link>
       </main>
     </div>
