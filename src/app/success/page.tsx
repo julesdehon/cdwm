c0c4cab2665f5b8aed3d97dd01015f6513925e46
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2024-11-19 23:22:39 +0000

    Fix success page width

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -9,8 +9,13 @@ export default function SuccessPage() {
           <h1>Success!</h1>
           <p>You have successfully registered your choices from Come Dine With Me! We look forward to welcoming you at The Westmark</p>
         </div>
-        <img src={"https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3Y3ZmZ2M3RndTNwaHdocTAxcTdhODk0ZWFvcnlvY2ZtNWF4NXF1biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13LlAxmDwAkopO/giphy.gif"}  alt={"A GIF showing a happy chef"}/>
-        <Link style={{display: "block", margin: "20px auto"}} className={styles.button} href={"/"}>Back Home</Link>
+        <div className={styles.maxWidthContainter}>
+          <img
+            src={"https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3Y3ZmZ2M3RndTNwaHdocTAxcTdhODk0ZWFvcnlvY2ZtNWF4NXF1biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13LlAxmDwAkopO/giphy.gif"}
+            alt={"A GIF showing a happy chef"}
+          style={{width: "500px", maxWidth: "90%"}}/>
+          <Link style={{display: "block", margin: "20px auto"}} className={styles.button} href={"/"}>Back Home</Link>
+        </div>
       </main>
     </div>
   )
